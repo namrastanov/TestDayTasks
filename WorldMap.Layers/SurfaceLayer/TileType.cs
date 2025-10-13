95867cf5a47ce2b4d95f9560b9c6610216759663
Nijat Amrastanov <namrastanov@gmail.com>
2025-10-13 12:55:32 +0400

    initial commit

---
+++ WorldMap.Layers/SurfaceLayer/TileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldMap.Layers.SurfaceLayer
+{
+    enum TileType : byte
+    {
+        Plain = 0,
+        Mountain = 1
+    }
+}
