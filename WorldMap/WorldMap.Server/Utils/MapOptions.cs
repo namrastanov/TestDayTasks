063c7228d2771063d1f84e190f95436140d5717e
Nijat Amrastanov <namrastanov@gmail.com>
2025-10-14 15:55:10 +0400

    integrated regions generation

---
+++ WorldMap/WorldMap.Server/Utils/MapOptions.cs
@@ -0,0 +1,11 @@
+namespace WorldMap.Server.Utils
+{
+    public sealed class MapOptions
+    {
+        public int Width { get; set; } = 1000;
+        public int Height { get; set; } = 1000;
+        public int MinimumRegions { get; set; } = 100;
+    }
+}
+
+
